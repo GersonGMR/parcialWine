c13377875672e4e3bd15577ea0c88539ed82da4e
GersonGMR <ggerson@live.com.ar>
2020-08-28 12:55:02 -0600

    updating test

--- parcialWine/mediaWine.py
+++ parcialWine/mediaWine.py
@@ -3,34 +3,34 @@ import pandas as pd
 df = pd.read_csv('winequality-red.csv', sep=';')
 df.head()
 
-media_alcohol = df.alcohol.median()
+alcohol_median = df.alcohol.median()
 for wine, alcohol in enumerate(df.alcohol):
-    if alcohol >= media_alcohol:
+    if alcohol >= alcohol_median:
         df.loc[wine, 'alcohol'] = 'high'
     else:
-        df.loc[wine, 'alcohol'] = 'down'
+        df.loc[wine, 'alcohol'] = 'low'
 df.groupby('alcohol').quality.mean()
 
-media_ph = df.pH.median()
+ph_median = df.pH.median()
 for wine, pH in enumerate(df.pH):
-    if pH >= media_ph:
+    if pH >= ph_median:
         df.loc[wine, 'pH'] = 'high'
     else:
-        df.loc[wine, 'pH'] = 'down'
+        df.loc[wine, 'pH'] = 'low'
 df.groupby('pH').quality.mean()
 
-media_residual_sugar = df.residual_sugar.median()
+residual_sugar_median = df.residual_sugar.median()
 for wine, residual_sugar in enumerate(df.residual_sugar):
-    if residual_sugar >= media_residual_sugar:
+    if residual_sugar >= residual_sugar_median:
         df.loc[wine, 'residual sugar'] = 'high'
     else:
-        df.loc[wine, 'residual sugar'] = 'down'
+        df.loc[wine, 'residual sugar'] = 'low'
 df.groupby('residual sugar').quality.mean()
 
-media_citric_acid = df.citric_acid.median()
+citric_acid_median = df.citric_acid.median()
 for wine, citric_acid in enumerate(df.citric_acid):
-    if citric_acid >= media_citric_acid:
+    if citric_acid >= citric_acid_median:
         df.loc[wine, 'citric acid'] = 'high'
     else:
-        df.loc[wine, 'citric acid'] = 'down'
+        df.loc[wine, 'citric acid'] = 'low'
 df.groupby('citric acid').quality.mean()
